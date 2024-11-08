ad11c246f43fcbf354a8d5ff5a42e78f881e7367
Bernie Sumption <bernie@berniecode.com>
2024-11-08 09:05:22 +0000

    AG-12850 [Theming API] bundle less color schemes in default colorSchemeVariable

--- packages/ag-grid-community/src/theming/parts/color-scheme/color-schemes.ts
+++ packages/ag-grid-community/src/theming/parts/color-scheme/color-schemes.ts
@@ -19,26 +19,22 @@ export const colorSchemeLight =
     /*#__PURE__*/
     createPart('colorScheme', 'light').withParams(defaultColorParams);
 
-const lightWarmParams = {
-    ...defaultColorParams,
-    foregroundColor: '#000000de',
-    borderColor: '#60300026',
-    chromeBackgroundColor: '#60300005',
-} as const;
-
 export const colorSchemeLightWarm =
     /*#__PURE__*/
-    createPart('colorScheme', 'lightWarm').withParams(lightWarmParams);
-
-const lightColdParams = {
-    ...defaultColorParams,
-    foregroundColor: '#000',
-    chromeBackgroundColor: '#f3f8f8',
-} as const;
+    createPart('colorScheme', 'lightWarm').withParams({
+        ...defaultColorParams,
+        foregroundColor: '#000000de',
+        borderColor: '#60300026',
+        chromeBackgroundColor: '#60300005',
+    });
 
 export const colorSchemeLightCold =
     /*#__PURE__*/
-    createPart('colorScheme', 'lightCold').withParams(lightColdParams);
+    createPart('colorScheme', 'lightCold').withParams({
+        ...defaultColorParams,
+        foregroundColor: '#000',
+        chromeBackgroundColor: '#f3f8f8',
+    });
 
 const darkParams = {
     backgroundColor: 'hsl(217, 0%, 17%)',
@@ -55,20 +51,18 @@ export const colorSchemeDark =
     /*#__PURE__*/
     createPart('colorScheme', 'dark').withParams(darkParams);
 
-const darkWarmParams = {
-    backgroundColor: 'hsl(29, 10%, 17%)',
-    foregroundColor: '#FFF',
-    chromeBackgroundColor: {
-        ref: 'foregroundColor',
-        mix: 0.05,
-        onto: 'backgroundColor',
-    },
-    browserColorScheme: 'dark',
-} as const;
-
 export const colorSchemeDarkWarm =
     /*#__PURE__*/
-    createPart('colorScheme', 'darkWarm').withParams(darkWarmParams);
+    createPart('colorScheme', 'darkWarm').withParams({
+        backgroundColor: 'hsl(29, 10%, 17%)',
+        foregroundColor: '#FFF',
+        chromeBackgroundColor: {
+            ref: 'foregroundColor',
+            mix: 0.05,
+            onto: 'backgroundColor',
+        },
+        browserColorScheme: 'dark',
+    });
 
 const darkBlueParams = {
     backgroundColor: '#1f2836',
@@ -89,8 +83,5 @@ export const colorSchemeVariable =
     /*#__PURE__*/
     createPart('colorScheme', 'variable')
         .withParams(defaultColorParams, 'light')
-        .withParams(lightWarmParams, 'light-warm')
-        .withParams(lightColdParams, 'light-cold')
         .withParams(darkParams, 'dark')
-        .withParams(darkWarmParams, 'dark-warm')
         .withParams(darkBlueParams, 'dark-blue');

a08c3f91fe32ddb0f5ca1e83c7d73e251b6ba88b
seanlandsman <sean@thelandsmans.com>
2024-11-20 15:44:58 +0000

    AG-11585 Update docs

---
+++ documentation/ag-grid-docs/src/content/docs/components/_examples/register/provided/modules/vue3/main.js
@@ -0,0 +1,57 @@
+import { createApp, ref, shallowRef } from 'vue';
+
+import { AllCommunityModule, ClientSideRowModelModule, ModuleRegistry } from 'ag-grid-community';
+import { AgGridVue } from 'ag-grid-vue3';
+
+import MedalRenderer from './medalRenderer.js';
+import './styles.css';
+
+ModuleRegistry.registerModules([AllCommunityModule, ClientSideRowModelModule]);
+
+const VueExample = {
+    template: `
+        <div style="height: 100%">
+            <div class="example-wrapper">
+                <ag-grid-vue
+                    style="width: 100%; height: 100%;"
+                    :columnDefs="columnDefs"
+                    @grid-ready="onGridReady"
+                    :defaultColDef="defaultColDef"
+                    :rowData="rowData">
+                </ag-grid-vue>
+            </div>
+        </div>
+    `,
+    components: {
+        'ag-grid-vue': AgGridVue,
+        medalRenderer: MedalRenderer,
+    },
+    setup(props) {
+        const columnDefs = ref([
+            {
+                headerName: 'Component By Name',
+                field: 'country',
+                cellRenderer: 'medalRenderer',
+            },
+        ]);
+
+        const defaultColDef = ref({
+            flex: 1,
+            minWidth: 100,
+        });
+
+        const rowData = ref(null);
+
+        fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+            .then((resp) => resp.json())
+            .then((data) => (rowData.value = data));
+
+        return {
+            columnDefs,
+            defaultColDef,
+            rowData,
+        };
+    },
+};
+
+createApp(VueExample).mount('#app');

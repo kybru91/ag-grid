46a47793c3980b30ec70dcfcaa24bad64e071240
seanlandsman <sean@thelandsmans.com>
2024-11-25 19:35:40 +0000

    AG-11585 Fix examples

--- documentation/ag-grid-docs/src/content/docs/column-groups/_examples/inner-header-group-component/customInnerHeaderGroup_vue.js
+++ documentation/ag-grid-docs/src/content/docs/column-groups/_examples/inner-header-group-component/customInnerHeaderGroup_vue.js
@@ -4,9 +4,5 @@ export default {
         <i v-if="params.icon" class="fa" :class="params.icon"></i>
         <span>{{ params.displayName }}</span>
       </div>
-    `,
-    data: function () {},
-    beforeMount() {},
-    mounted() {},
-    methods: {},
+    `
 };

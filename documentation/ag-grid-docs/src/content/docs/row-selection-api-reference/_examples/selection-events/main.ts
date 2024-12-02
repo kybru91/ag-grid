8bf042cfbb621519ad6fa165bfa91cc6a8e3d190
Elias Malik <elias.malik@ag-grid.com>
2024-12-02 10:07:49 +0000

    AG-13537 Remove header checkbox from example

--- documentation/ag-grid-docs/src/content/docs/row-selection-api-reference/_examples/selection-events/main.ts
+++ documentation/ag-grid-docs/src/content/docs/row-selection-api-reference/_examples/selection-events/main.ts
@@ -24,7 +24,7 @@ const gridOptions: GridOptions<IOlympicData> = {
         flex: 1,
         minWidth: 100,
     },
-    rowSelection: { mode: 'multiRow' },
+    rowSelection: { mode: 'multiRow', headerCheckbox: false },
     onRowSelected,
     onSelectionChanged,
 };

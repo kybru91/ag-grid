80c0db8db328c81fea4b62af2a3742f3a79c6e52
Mark Durrant <mark.j.durrant@gmail.com>
2024-12-06 16:04:46 +0000

    Ag 13351 homepage updates (#9354)
    * Minor copy changes
    
    * Update showcase
    
    * Header typewriter animation
    
    * AG-13351 - FW animation minor tidy up
    
    * AG-13351 - FW animation change to rotation
    
    ---------
    
    Co-authored-by: James Swinton-Bland <james_swinton@hotmail.co.uk>

--- documentation/ag-grid-docs/src/components/framework-landing-pages/react/sections/showcase/Showcase.tsx
+++ documentation/ag-grid-docs/src/components/framework-landing-pages/react/sections/showcase/Showcase.tsx
@@ -1,80 +1,66 @@
-import { Icon } from '@ag-website-shared/components/icon/Icon';
 import React from 'react';
 
 import styles from './Showcase.module.scss';
-import Aerie from './images/aerie.svg?react';
+import AerospaceIcon from './images/aerospace.svg?react';
 import AIIcon from './images/ai.svg?react';
 import DashboardIcon from './images/dashboard.svg?react';
 import FinanceIcon from './images/finance.svg?react';
-import MLFlowLogo from './images/mlflow.svg?react';
+import JPMorgan from './images/jpmorgan.svg?react';
+import Microsoft from './images/microsoft.svg?react';
 import ModelIcon from './images/model.svg?react';
-import OpenBB from './images/openbb.svg?react';
+import MongoDB from './images/mongodb.svg?react';
+import Nasa from './images/nasa.svg?react';
 import RA from './images/ra.svg?react';
-import TerminalIcon from './images/terminal.svg?react';
-import Windmill from './images/windmill.svg?react';
 
 const SHOWCASE_ITEMS = [
     {
         title: 'Finance',
         titleIcon: <FinanceIcon />,
         description:
-            'Analyse complex financial data, perform calculations and visualise the data in AG Grid, with standalone charts from AG Charts.',
-        projectName: 'OpenBB',
-        projectLogo: <OpenBB />,
-        projectHref: 'https://openbb.co/',
-        sourceHref: 'https://github.com/OpenBB-finance/OpenBBTerminal',
+            "Salt is J.P. Morgan's open-source design system for financial services and other industries, which uses AG Grid for its Data Grid component.",
+        projectName: 'J.P. Morgan',
+        projectLogo: <JPMorgan />,
+        projectHref: 'https://www.saltdesignsystem.com/salt/index',
     },
     {
         title: 'ML/AI',
         titleIcon: <AIIcon />,
         description:
-            'Build models and generative AI apps on a unified, end-to-end, MLOps platform which uses AG Grid to power the tables in its dashboard',
-        projectName: 'MLflow',
-        projectLogo: <MLFlowLogo />,
-        projectHref: 'https://mlflow.org',
-        sourceHref: 'https://github.com/mlflow/mlflow/tree/master',
+            'MSR Gamut is a design probe that leverages AG Grid to help data scientists effectively visualize and understand their Machine Learning models and data.',
+        projectName: 'Microsoft',
+        projectLogo: <Microsoft />,
+        projectHref: 'https://microsoft.github.io/msrgamut/',
     },
     {
-        title: 'Developer Platforms',
+        title: 'Databases',
         titleIcon: <ModelIcon />,
         description:
-            'Open-source developer platforms and workflow engines who use AG Grid as part of their drag & drop UI builder.',
-        projectName: 'Windmill',
-        projectLogo: <Windmill />,
-        projectHref: 'https://www.windmill.dev/docs/apps/app_configuration_settings/aggrid_table#aggird-table-input',
-        sourceHref: 'https://github.com/windmill-labs/windmill',
+            "Compass is the GUI for MongoDB that uses AG Grid to visualize and manage users' databases, providing an intuitive interface to explore and interact with data.",
+        projectName: 'MongoDB',
+        projectLogo: <MongoDB />,
+        projectHref: 'https://www.mongodb.com/products/tools/compass',
     },
     {
-        title: 'Dashboards',
+        title: 'Aerospace',
+        titleIcon: <AerospaceIcon />,
+        description:
+            'NASA AMMOS is a tool for planning, scheduling, and sequencing tools for modern space missions that uses AG Grid to help visualise mission data.',
+        projectName: 'NASA',
+        projectLogo: <Nasa />,
+        projectHref: 'https://nasa-ammos.github.io/aerie-docs/',
+    },
+    {
+        title: 'Developer Tools',
         titleIcon: <DashboardIcon />,
         description:
-            'An open source React library for building dashboards, with AG Grid Enterprise support for building React tables with advanced features.',
+            'React Admin is an open source framework for building admin interfaces with React which uses AG Grid for its Data Grid functionality.',
         projectName: 'React Admin',
         projectLogo: <RA />,
-        projectHref: 'https://marmelab.com/react-admin/DatagridAG.html',
-        sourceHref: 'https://github.com/marmelab/react-admin',
-    },
-    {
-        title: 'Data Modelling',
-        titleIcon: <TerminalIcon />,
-        description:
-            'Planning, scheduling, and sequencing tools for modern space missions. AG Grid is used throughout to help visualise mission data.',
-        projectName: 'Aerie',
-        projectLogo: <Aerie />,
-        projectHref: 'https://nasa-ammos.github.io/aerie-docs/',
-        sourceHref: 'https://github.com/NASA-AMMOS/aerie-ui',
+        projectHref: 'https://marmelab.com/react-admin/',
     },
 ];
 
-const ShowcaseItem: React.FC = ({
-    title,
-    titleIcon,
-    description,
-    projectName,
-    projectLogo,
-    projectHref,
-    sourceHref,
-}) => {
+const ShowcaseItem: React.FC = ({ title, titleIcon, description, projectName, projectLogo, projectHref }) => {
     return (
         <div className={styles.showcaseGridItem}>
             <div className={styles.showcaseIconWrapper}>
@@ -83,16 +69,12 @@ const ShowcaseItem: React.FC = ({
             </div>
             <h3 className={styles.showcaseTitle}>
                 {title}
-                <span className={styles.showcaseName}> — {projectName}</span>
+                <span className={styles.showcaseName}></span>
             </h3>
             <p className={styles.showcaseDescription}>{description}</p>
             <div className={styles.showcaseLinksWrapper}>
                 <a href={projectHref} target="_blank" rel="noopener noreferrer">
                     Visit {projectName}
-                </a>{' '}
-                <a href={sourceHref} className={styles.githubLink} target="_blank" rel="noopener noreferrer">
-                    <Icon name="github" />
-                    View source
                 </a>
             </div>
         </div>

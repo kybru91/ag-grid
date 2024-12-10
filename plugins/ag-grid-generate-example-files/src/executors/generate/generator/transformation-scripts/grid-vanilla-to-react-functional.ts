95e86824675fbd37af07e16ade43e142cf2aeec0
Stephen Cooper <scooperdev@gmail.com>
2024-12-10 16:10:08 +0000

    RTI-2505  (#9477)
    * RTI-2505 React Js Fix
    
    * Js version

--- plugins/ag-grid-generate-example-files/src/executors/generate/generator/transformation-scripts/grid-vanilla-to-react-functional.ts
+++ plugins/ag-grid-generate-example-files/src/executors/generate/generator/transformation-scripts/grid-vanilla-to-react-functional.ts
@@ -256,7 +256,7 @@ export function vanillaToReactFunctional(
             componentProps.push('onGridReady={onGridReady}');
         } else {
             // We need to check if we need to add integrated dark mode code for and example that does not have data or onGridReady
-            darkModeWithGridRef = getIntegratedDarkModeCode(bindings.exampleName, true, 'gridRef.current?.api?');
+            darkModeWithGridRef = getIntegratedDarkModeCode(bindings.exampleName, false, 'gridRef.current?.api?');
         }
         componentProps.push(...componentEventAttributes);
 
@@ -331,7 +331,7 @@ const GridExample = () => {
     const gridRef = useRef();
     ${stateProperties.join('\n    ')}
 
-${gridReady}
+${gridReady}${darkModeWithGridRef ? '\n' + darkModeWithGridRef : ''}
 
 ${[].concat(eventHandlers, externalEventHandlers, instanceMethods).join('\n\n   ')}
 

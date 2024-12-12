63f80e8dada7251338188f7dac4d0d3b41ce7a67
seanlandsman <sean@thelandsmans.com>
2024-12-12 11:36:14 +0000

    AG-13684 Set grid events/callbacks as optional

--- packages/ag-grid-vue3/updateGridAndColumnProperties.cjs
+++ packages/ag-grid-vue3/updateGridAndColumnProperties.cjs
@@ -100,7 +100,7 @@ function generatePropsAndEmits({ typeLookup, eventTypeLookup, docLookup }) {
             eventsToWrite.push({ order, line });
             eventsPropTypesWrite.push({
                 order,
-                line: `   '${kebabNameToAttrEventName(kebabProperty(event))}': ${eventType},${EOL}`,
+                line: `   '${kebabNameToAttrEventName(kebabProperty(event))}'?: ${eventType},${EOL}`,
             });
         } else {
             missingEventTypes.push(event);

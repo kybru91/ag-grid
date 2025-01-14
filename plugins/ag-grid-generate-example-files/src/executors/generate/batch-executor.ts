ebc2ceebdcbc7cb46433a8ada83e31fd52137596
Alan Treadway <alan.treadway@ag-grid.com>
2025-01-14 16:42:24 +0000

    Enable multi-core example generation when batching.

--- plugins/ag-grid-generate-example-files/src/executors/generate/batch-executor.ts
+++ plugins/ag-grid-generate-example-files/src/executors/generate/batch-executor.ts
@@ -1,7 +1,10 @@
-import { batchExecutor } from 'ag-shared/plugin-utils';
+import { batchWorkerExecutor } from 'ag-shared/plugin-utils';
+import { versions } from 'process';
 
-import { generateFiles } from './executor';
-
-const executor = batchExecutor(generateFiles);
+if (versions.node < '18.18') {
+    // eslint-disable-next-line no-console
+    throw new Error('Upgrade Node.js to v18.18.0 for multi-threaded thumbnail generation; found: ' + versions.node);
+}
+const executor = batchWorkerExecutor(`${module.path}/batch-instance.js`);
 
 export default executor;
---
+++ plugins/ag-grid-generate-example-files/src/executors/generate/batch-instance.ts
@@ -0,0 +1,25 @@
+import type { ExecutorContext } from '@nx/devkit';
+
+import type { BatchExecutorTaskResult } from '../../executors-utils';
+import type { ExecutorOptions } from './executor';
+import generateFiles from './executor';
+
+export type Message = {
+    taskName: string;
+    options: ExecutorOptions;
+    context: ExecutorContext;
+};
+
+export default async function processor(msg: Message) {
+    const { options, taskName } = msg;
+
+    let result: BatchExecutorTaskResult;
+    try {
+        await generateFiles(options);
+        result = { task: taskName, result: { success: true, terminalOutput: '' } };
+    } catch (e) {
+        result = { task: taskName, result: { success: false, terminalOutput: `${e}` } };
+    }
+
+    return result;
+}

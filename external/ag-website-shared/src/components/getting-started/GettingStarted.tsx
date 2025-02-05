c0e29fb84e4db092dd368be82171805580149825
Kyler Phillips <kylerphillips@MacBook-Pro.local>
2025-02-05 11:05:33 +0000

    AG-12914 CSS tweaks to getting started blocks

--- external/ag-website-shared/src/components/getting-started/GettingStarted.tsx
+++ external/ag-website-shared/src/components/getting-started/GettingStarted.tsx
@@ -1,39 +1,50 @@
+import { gridUrlWithPrefix } from '@ag-website-shared/utils/gridUrlWithPrefix';
+import { useFrameworkFromStore } from '@utils/hooks/useFrameworkFromStore';
 import type { FunctionComponent } from 'react';
 
 import { Icon } from '../icon/Icon';
 import styles from './getting-started.module.scss';
 
 const GettingStarted: FunctionComponent = () => {
+    const framework = useFrameworkFromStore();
+
     const features = [
         {
             icon: 'concepts',
             title: 'Key Concepts',
             description: 'Browse our commonly used features',
-            link: '../quick-reference/',
+            link: './quick-reference/',
         },
         {
             icon: 'tutorials',
             title: 'Tutorials',
             description: 'Features, themes and more',
-            link: '../deep-dive/',
+            link: './deep-dive/',
         },
         {
             icon: 'communityEnterprise',
             title: 'Community & Enterprise',
             description: 'Compare the differences between versions',
-            link: '../community-vs-enterprise/',
+            link: './community-vs-enterprise/',
         },
     ];
 
     return (
         <div className={styles.container}>
             {features.map((feature, index) => (
-                <a href={feature.link}>
+                <a href={gridUrlWithPrefix({ framework, url: feature.link })}>
                     <div key={index} className={styles.card}>
-                        <Icon name={feature.icon} className={styles.icon}>
-                            {feature.icon}
-                        </Icon>
-                        <h3 className={styles.title}>{feature.title}</h3>
+                        <div className={styles.iconGroup}>
+                            <Icon name={feature.icon} className={styles.icon}>
+                                {feature.icon}
+                            </Icon>
+                        </div>
+                        <div className={styles.titleIcon}>
+                            <h3 className={styles.title}>{feature.title}</h3>
+                            <div>
+                                <Icon name="chevronRight" className={`${styles.icon} ${styles.arrowRight}`}></Icon>
+                            </div>
+                        </div>
                         <p className={styles.description}>{feature.description}</p>
                     </div>
                 </a>
